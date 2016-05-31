de9043f0f49b6aac5c6995873b9477db90a0ab6a
Peter Hague <pete.hague@gmail.com>
2016-05-31 16:18:05 +0100

    Single image processing file

---
+++ process.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+target = sys.argv[1]
+coords = open(target+"/coords.txt", "r")
+line = coords.readline()
+data = re.findall("[0-9.]+", line)
+ay = data[0]
+by = data[1]
+ax = data[2]
+bx = data[3]
+coords.close()
+
+cyfrp.run(target, 0, ay, by, ax, bx)
